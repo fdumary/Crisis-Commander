86c56353bc03e841a4d593bf3d42455981c73257
Nicholas-Cuc <lobocuc@gmail.com>
2024-10-18 22:24:43 -0400

    forms and feedback basic functions

--- backend/main.py
+++ backend/main.py
@@ -19,14 +19,26 @@ response = supabase.schema("public").table("feedback").select("*").execute()
 print("Response: ", response)
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return 'Hello World!'
+"""@app.route('/')
+def hello():
+    return 'Hello World!'
+    """
 # # TODO: Login
 # # TODO: Log_Out
 # # TODO: Add_Faculty
+@app.route('/forms', methods=['GET'])
+def forms():
+    data = supabase.table("forms").select("*").execute()
+    return print(data)
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    data = supabase.table("feedback").select("*").execute()
+    return print(data)
+def get_feedback():
+    data = supabase.table("feedback").select("*").execute()
+    return data
 # # TODO: Add_Patient
 # # TODO: View_Patients
 # # TODO: View_Faculty
